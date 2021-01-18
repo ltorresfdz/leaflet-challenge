b2d265c7445c4f625c043a54f64e24e32631bca9
Laura Torres <ltorresfdz@gmail.com>
2021-01-17 21:49:45 -0600

    85% work on Leaf Part 1

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,81 +1,89 @@
+// Creating our initial map object
+// We set the longitude, latitude, and the starting zoom level
+// This gets inserted into the div with an id of 'map'
+var myMap = L.map("map", {
+    center: [15.5994, -28.6731],
+    zoom: 3
+  });
+
+
+  // Adding a tile layer (the background map image) to our map
+  // We use the addTo method to add objects to our map
+  L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "light-v10",
+    accessToken: API_KEY
+  }).addTo(myMap);
+
 // Store our API endpoint inside queryUrl
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
-  
   console.log(data);
-  console.log("aqui termina");
+  console.log("finishing reading dataset");
   console.log(data.features[0]);
   console.log(data.features[0].geometry.coordinates[0]); //longitud
   console.log(data.features[0].geometry.coordinates[1]); // latitude
   console.log(data.features[0].properties.place); // place of earthquake
   console.log(data.features[0].properties.mag); // magnitude
-    // Once we get a response, send the data.features object to the createFeatures function
-  //createFeatures(data.features);
-})  
+ });
 
-// Creating our initial map object
-// We set the longitude, latitude, and the starting zoom level
-// This gets inserted into the div with an id of 'map'
-var myMap = L.map("map", {
-    center: [37.09, -95.71],
-    zoom: 5
-  });
+
+// Grabbing our GeoJSON data..
+d3.json(queryUrl, function(data) {
+    //function to select circle  color depending earthquake magnitude
+    function chooseColor(magnitude) {
+        switch (true) {
+        case magnitude > 5:
+            return "#ea2c2c";
+        case magnitude >4:
+            return "#ea822c";
+        case magnitude >3:
+            return "#ee9c00";
+        case magnitude >2:
+            return "#eecc00";
+        case magnitude >1:
+            return "#d4ee00";
+        default:
+            return "#98ee00";
+            }
+        };
+     //function to select circle size depending earthquake magnitude
+        function styleInfo(feature) {
+            return {
+              opacity: 1,
+              fillOpacity: 1,
+              fillColor: chooseColor(feature.properties.mag),
+              color: "#000000",
+              radius: getSize(feature.properties.mag),
+              stroke: true,
+              weight: 0.5
+            };
+        };
+   //function to select circle  size depending earthquake magnitude
+        function getSize(magnitude) {
+            if (magnitude === 0) {
+              return 1;
+            };
+        
+            return magnitude * 4;
+          };
   
-  // Adding a tile layer (the background map image) to our map
-  // We use the addTo method to add objects to our map
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
-  }).addTo(myMap);
-
-  // Define a markerSize function that will give each city a different radius based on its population
-function markerSize(population) {
-    return population /40;
-  }
-
-  // Loop through the cities array and create one marker for each city object
-for (var i = 0; i < cities.length; i++) {
-    L.circle(cities[i].location, {
-      fillOpacity: 0.75,
-      color: "white",
-      fillColor: "purple",
-      // Setting our circle's radius equal to the output of our markerSize function
-      // This will make our marker's size proportionate to its population
-      radius: markerSize(cities[i].population)
-    }).bindPopup("<h1>" + cities[i].name + "</h1> <hr> <h3>Population: " + cities[i].population + "</h3>").addTo(myMap);
-  }
-
-
-  / Loop through the cities array and create one marker for each city object
-for (var i = 0; i < countries.length; i++) {
-
-  // Conditionals for countries points
-  var color = "";
-  if (countries[i].points > 200) {
-    color = "yellow";
-  }
-  else if (countries[i].points > 100) {
-    color = "blue";
-  }
-  else if (countries[i].points > 90) {
-    color = "green";
-  }
-  else {
-    color = "red";
-  }
-
-  // Add circles to map
-  L.circle(countries[i].location, {
-    fillOpacity: 0.75,
-    color: "white",
-    fillColor: color,
-    // Adjust radius
-    radius: countries[i].points * 1500
-  }).bindPopup("<h1>" + countries[i].name + "</h1> <hr> <h3>Points: " + countries[i].points + "</h3>").addTo(myMap);
-}
+    // Creating a geoJSON layer with the retrieved data
+  L.geoJson(data, {
+    // Style each feature (in this case earthquake magnitude)
+       pointToLayer: function(feature, latlng) {
+          return L.circleMarker(latlng);
+        },
+        // circle size and color
+        style: styleInfo,
+        // popup for each marker
+        onEachFeature: function(feature, layer) {
+          layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+        }
+      }).addTo(myMap);
+    });
