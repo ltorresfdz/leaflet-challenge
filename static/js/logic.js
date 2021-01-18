149db9ce50d661b1a0926991bd6e511d534bd300
Laura Torres <ltorresfdz@gmail.com>
2021-01-17 19:53:56 -0600

    initial work on logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -3,72 +3,79 @@ var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_we
 
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
-  // Once we get a response, send the data.features object to the createFeatures function
-  createFeatures(data.features);
-});
+  
+  console.log(data);
+  console.log("aqui termina");
+  console.log(data.features[0]);
+  console.log(data.features[0].geometry.coordinates[0]); //longitud
+  console.log(data.features[0].geometry.coordinates[1]); // latitude
+  console.log(data.features[0].properties.place); // place of earthquake
+  console.log(data.features[0].properties.mag); // magnitude
+    // Once we get a response, send the data.features object to the createFeatures function
+  //createFeatures(data.features);
+})  
 
-function createFeatures(earthquakeData) {
-
-  // Define a function we want to run once for each feature in the features array
-  // Give each feature a popup describing the place and time of the earthquake
-  function onEachFeature(feature, layer) {
-    layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-  }
-
-  // Create a GeoJSON layer containing the features array on the earthquakeData object
-  // Run the onEachFeature function once for each piece of data in the array
-  var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
+// Creating our initial map object
+// We set the longitude, latitude, and the starting zoom level
+// This gets inserted into the div with an id of 'map'
+var myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 5
   });
-
-  // Sending our earthquakes layer to the createMap function
-  createMap(earthquakes);
-}
-
-function createMap(earthquakes) {
-
-  // Define streetmap and darkmap layers
-  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  
+  // Adding a tile layer (the background map image) to our map
+  // We use the addTo method to add objects to our map
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
     id: "mapbox/streets-v11",
     accessToken: API_KEY
-  });
-
-  var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "dark-v10",
-    accessToken: API_KEY
-  });
-
-  // Define a baseMaps object to hold our base layers
-  var baseMaps = {
-    "Street Map": streetmap,
-    "Dark Map": darkmap
-  };
-
-  // Create overlay object to hold our overlay layer
-  var overlayMaps = {
-    Earthquakes: earthquakes
-  };
-
-  // Create our map, giving it the streetmap and earthquakes layers to display on load
-  var myMap = L.map("map", {
-    center: [
-      37.09, -95.71
-    ],
-    zoom: 5,
-    layers: [streetmap, earthquakes]
-  });
-
-  // Create a layer control
-  // Pass in our baseMaps and overlayMaps
-  // Add the layer control to the map
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
   }).addTo(myMap);
+
+  // Define a markerSize function that will give each city a different radius based on its population
+function markerSize(population) {
+    return population /40;
+  }
+
+  // Loop through the cities array and create one marker for each city object
+for (var i = 0; i < cities.length; i++) {
+    L.circle(cities[i].location, {
+      fillOpacity: 0.75,
+      color: "white",
+      fillColor: "purple",
+      // Setting our circle's radius equal to the output of our markerSize function
+      // This will make our marker's size proportionate to its population
+      radius: markerSize(cities[i].population)
+    }).bindPopup("<h1>" + cities[i].name + "</h1> <hr> <h3>Population: " + cities[i].population + "</h3>").addTo(myMap);
+  }
+
+
+  / Loop through the cities array and create one marker for each city object
+for (var i = 0; i < countries.length; i++) {
+
+  // Conditionals for countries points
+  var color = "";
+  if (countries[i].points > 200) {
+    color = "yellow";
+  }
+  else if (countries[i].points > 100) {
+    color = "blue";
+  }
+  else if (countries[i].points > 90) {
+    color = "green";
+  }
+  else {
+    color = "red";
+  }
+
+  // Add circles to map
+  L.circle(countries[i].location, {
+    fillOpacity: 0.75,
+    color: "white",
+    fillColor: color,
+    // Adjust radius
+    radius: countries[i].points * 1500
+  }).bindPopup("<h1>" + countries[i].name + "</h1> <hr> <h3>Points: " + countries[i].points + "</h3>").addTo(myMap);
 }
